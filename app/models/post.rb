8a1b51fc898c7c7cdec67948f8e48adf247ee41d
Guy Archibald <goarchib83@gmail.com>
2012-07-25 14:26:41 -0500

    first commit

---
+++ app/models/post.rb
@@ -0,0 +1,9 @@
+class Post < ActiveRecord::Base
+  attr_accessible :content, :name, :title
+  
+  validates :name,  :presence => true
+  validates :title, :presence => true
+                    
+	
+  has_many :comments, :dependent => :destroy
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = '64cb362d3bfd041682680928b3f3a8e36fc3c9997aa68d0bd27fb4fc73231c82fbdfb011b22d02f5a98cce8c8aa3e48ae7fece38bc8d8044276f00a810b58661'
